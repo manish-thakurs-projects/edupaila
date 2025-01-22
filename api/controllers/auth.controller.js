52393031a804baf28efe8170d65939b40d35ae2f
manish thakur <emailmanishthakur@gmail.com>
2025-01-22 22:38:42 +0545

    signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
 
-// Password strength validation functions
+
 const passwordLengthValidator = (password) => password.length >= 8;
 const passwordUppercaseValidator = (password) => /[A-Z]/.test(password);
 const passwordNumberValidator = (password) => /\d/.test(password);
@@ -11,7 +13,6 @@ const passwordSpecialCharacterValidator = (password) => /[@$!%*?&]/.test(passwor
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
-    // Validate input fields
     if (!username || username.trim() === '') {
         return next(errorHandler(400, 'Username must be provided'));
     }
@@ -24,7 +25,6 @@ export const signup = async (req, res, next) => {
         return next(errorHandler(400, 'Password must be provided'));
     }
 
-    // Password validation
     if (!passwordLengthValidator(password)) {
         return next(errorHandler(400, 'Password must be at least 8 characters long.'));
     }
@@ -71,4 +71,40 @@ export const signup = async (req, res, next) => {
         console.error(error);
         return next(errorHandler(500, 'Something went wrong. Please try again later.'));
     }
+};
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'Wrong credentials'));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Wrong credentials'));
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+        expires: moment().add(1, 'months').toDate() 
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
