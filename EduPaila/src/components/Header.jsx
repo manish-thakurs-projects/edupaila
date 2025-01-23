8802f5845cf17cbceeed566cea17da26e9621ef7
manish thakur <emailmanishthakur@gmail.com>
2025-01-23 11:48:50 +0545

    header ui with profile

--- EduPaila/src/components/Header.jsx
+++ EduPaila/src/components/Header.jsx
@@ -1,63 +1,119 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
-import { Link, useLocation} from 'react-router-dom'
-import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon, FaUser } from 'react-icons/fa'
-import React from 'react'
-import './app.css'
-import Logo from './logo'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
+import Logo from '../components/logo'
+import { useEffect, useState } from 'react';
+
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
-    <Navbar className='border-b-2 p-2'>
-    <Logo/>
-    <form>
-      <TextInput
-      type='text'
-      placeholder='search'
-      rightIcon={AiOutlineSearch}
-      className='bar hidden lg:inline'
-      />
-    </form>
-    <Button className='btn w-36 h-10 lg:hidden' color='gray' pill>
-<AiOutlineSearch/>
-    </Button>
-    <div className='flex gap-2 md:order-2'>
-      <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-        <FaMoon/>
+    <Navbar className='border-b-2'>
+    <Logo/> 
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
       </Button>
-      <Link to='/signin'>
-      <Button  className='w-12 h-10' color='gray' pill outline gradientDuoTone='purpleToBlue'>
-      <FaUser />
-      </Button>
-      </Link>
-    </div>
-      <Navbar.Toggle/>
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
+      </div>
       <Navbar.Collapse>
-  <Navbar.Link
-    active={path === "/"}
-    as={'div'}
-    className="text-lg sm:text-l font-medium"
-  >
-    <Link to='/'>Home</Link>
-  </Navbar.Link>
-
-  <Navbar.Link
-    active={path === "/about"}
-    as={'div'}
-    className="text-lg sm:text-l font-medium"
-  >
-    <Link to='/about'>About</Link>
-  </Navbar.Link>
-
-  <Navbar.Link
-    active={path === "/books"}
-    as={'div'}
-    className="text-lg sm:text-l font-medium"
-  >
-    <Link to='/books'>Books</Link>
-  </Navbar.Link>
-</Navbar.Collapse>
-
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/books'} as={'div'}>
+          <Link to='/books'>Books</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     profilePicture: {
       type: String,
       default:
-        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        'https://static.vecteezy.com/system/resources/thumbnails/002/387/693/small_2x/user-profile-icon-free-vector.jpg',
     },
     isAdmin: {
       type: Boolean,
